714b89713c740a773f4f1678a015f0fc173b95cb
bethbenjamin <bethbenjamin@gmail.com>
2018-03-07 10:44:25 -0500

    first commit

---
+++ MovieDirectorApp/Models/Director.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieDirectorApp.Models
+{
+    public class Director
+    {
+        [Key]
+        public int DirectorID { get; set; }
+        public string Name { get; set; }
+
+
+        //navigation property
+
+        public virtual ICollection<Movie> Movies { get; set; }
+
+
+    }
+}
